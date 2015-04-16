ceea9ec2b3ab66a9c44fc4b785fa13478f66bacd
bdenardi <devnull@localhost>
2015-04-16 18:27:05 -0300

    Agregado html solouser, link inicio, link solouser para login y logout

--- sitio/views.py
+++ sitio/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 def inicio(request):
     return render(request, 'inicio.html', {})
+
+@login_required
+def solouser(request):
+    return render(request, 'solouser.html', {})
+
