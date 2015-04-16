8cce449da8cf85c890816fd62b892cdadbc00ded
fedehenzenn <devnull@localhost>
2015-04-16 18:01:41 -0300

    agregado modelo user

--- sitio/models.py
+++ sitio/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount
+
+import hashlib
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+    about_me = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return "{}'s profile'".format(self.user.username)
+
+    class Meta:
+        db_table = 'user_profile'
 
 
+    def account_verified(self):
+        if self.user.is_authenticated:
+            result = EmailAddress.objects.filter(email=self.user.email)
+            if len(result):
+                return result[0].verified
+        return False
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
