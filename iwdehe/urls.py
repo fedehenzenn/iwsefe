d3d974d88f7d2288e2d8e1eb874799b094e9fab0
fedehenzenn <devnull@localhost>
2015-04-16 19:00:15 -0300

    comentado

--- iwdehe/urls.py
+++ iwdehe/urls.py
@@ -8,6 +8,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^inicio/$', 'sitio.views.inicio'),
+#    url(r'^accounts/logout/$,', 'django.contrib.auth.view.logout',
+#    {'next_page': '/inicio'}),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^solouser/', 'sitio.views.solouser'),
 )
