345710d3e6e26e447e27aea73e8ee88d8cbf00e3
Fede Henzenn <fede.henzenn@gmail.com>
2015-04-23 10:32:38 -0300

    creado clase post

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+8
 # Create your models here.
+
+
+class post(models.Model):
+    titulo = models.CharField(max_length=50)
+    fecha = models.DateTimeField()
+    texto = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.titulo
