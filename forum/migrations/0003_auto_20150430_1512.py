5e5ba6c2cf9f341e7439899a67e091453d39159a
Fede Henzenn <fede.henzenn@gmail.com>
2015-04-30 12:14:44 -0300

    agregado clase categoria, foreignkey de gamereview categoria

---
+++ forum/migrations/0003_auto_20150430_1512.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('forum', '0002_gamereview_author'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Categoria',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='gamereview',
+            name='cat',
+            field=models.ForeignKey(blank=True, to='forum.Categoria', null=True),
+        ),
+    ]
