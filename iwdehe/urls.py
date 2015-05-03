3659882cb807bafd149a677301c1c1c3e8dc3309
BrunoD <bruno.denardi91@gmail.com>
2015-05-03 02:04:03 -0300

    Update urls.py
    Borrado de conflictos generados en urls.py

--- iwdehe/urls.py
+++ iwdehe/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-<<<<<<< HEAD
 from forum.views import *
-=======
-from forum.views import review_create, review_update, review_listing, review_delete, detail_review
->>>>>>> 645e25f75bf84b03bf7ceb31c22bc8e6c4ad0f11
+
 
 
 urlpatterns = patterns('',
@@ -18,13 +15,9 @@ urlpatterns = patterns('',
     url(r'^home/listreviews/$', review_listing.as_view(), name='listing'),
     url(r'^home/categorias/$', 'forum.views.categorias_listing', name='categorias'),
     url(r'^home/create/$', review_create.as_view(), name='create'),
-<<<<<<< HEAD
     url(r'^home/categorias/add/$', categoria_add.as_view(), name='categoria_add'),
     url(r'^home/listreviews/(?P<pk>\d+)/$', 'forum.views.detail_review', name='detail'),
     url(r'^home/listreviews/comment/(?P<pk>\d+)/$', 'forum.views.comentar', name='comment'),
-=======
-    url(r'^home/list/(?P<pk>\d+)/$', detail_review, name='detail'),
->>>>>>> 645e25f75bf84b03bf7ceb31c22bc8e6c4ad0f11
 #    url(r'^(?P\d+)/update$', review_update.as_view(), name='update'),
 #    url(r'^(?P\d+)/delete$', review_delete.as_view(), name='delete'),
     )
