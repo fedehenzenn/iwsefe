299185ec3d1248673dcc04933c39b56f8742014d
BrunoD <bruno.denardi91@gmail.com>
2015-05-03 02:05:34 -0300

    modificado conflicto ocurrido en forum/forms.py

--- forum/forms.py
+++ forum/forms.py
@@ -1,18 +1,7 @@
 from django.forms import ModelForm
 from forum.models import *
-<<<<<<< HEAD
-
-
 class ComentarioForm(ModelForm):
     class Meta:
         model = Comentario
         fields = ['text']
-=======
-from shared.utils import ContainerFormMixin
 
-
-class ComentarioForm(ContainerFormMixin, ModelForm):
-    class Meta:
-        model = ComentarioPost
-        exclude = ["author", "review"]
->>>>>>> 645e25f75bf84b03bf7ceb31c22bc8e6c4ad0f11
