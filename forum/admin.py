ed9ace424c053d2743477ef951bf13819bb68fe1
Fede Henzenn <fede.henzenn@gmail.com>
2015-05-16 17:19:23 -0300

    agregado admin para denuncia

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from forum.models import Denuncia
 
-# Register your models here.
+
+class DenunciaAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['review', 'visto_por']}),
+        ('Date information', {'fields': ['desc'], 'classes':['collapse']}),
+        ]
+
+    list_display = ['review']
+    list_filter = ['visto']
+
+admin.site.register(Denuncia, DenunciaAdmin)
