a583b2a9f59c5dfd690f0b4572e8408300c06f7b
Bruno91 <bruno.denardi91@gmail.com>
2015-05-16 20:54:51 -0300

    Agregado html

---
+++ forum/migrations/0006_denuncia.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('forum', '0005_auto_20150503_2215'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Denuncia',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('desc', models.TextField()),
+                ('date', models.DateTimeField()),
+                ('visto', models.BooleanField(default=False)),
+                ('review', models.ForeignKey(to='forum.Gamereview', null=True)),
+                ('visto_por', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+        ),
+    ]
