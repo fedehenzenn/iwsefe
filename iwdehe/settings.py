c0d81e2be5b18d043c405ef78d5285f9b0c58ce2
Fede Henzenn <fede.henzenn@gmail.com>
2015-05-18 17:16:06 -0300

    cambiado true

--- iwdehe/settings.py
+++ iwdehe/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'sitio/templates'),],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
