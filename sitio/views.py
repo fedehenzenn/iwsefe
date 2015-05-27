032cc90fbd0c06f608565cd5ab5bce03b5eeaa6b
Bruno91 <bruno.denardi91@gmail.com>
2015-05-27 02:36:47 -0300

    Agregado estilos a todas las paginas restantes, agregado de urls restricted, falta moderar esa parte

--- sitio/views.py
+++ sitio/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.contrib.admin.views.decorators import staff_member_required
+
 
 def home(request):
     return render(request, 'home.html')
 
 
-@staff_member_required
 def restricted(request):
     return render(request, 'restricted.html', {})
