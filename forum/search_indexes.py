b52eef9efe07c2aa8e03b79d00bb1298558d0147
Fede Henzenn <fede.henzenn@gmail.com>
2015-05-28 16:52:49 -0300

    agregado templates de busqueda

---
+++ forum/search_indexes.py
@@ -0,0 +1,15 @@
+from haystack import indexes
+from forum.models import Gamereview
+
+
+class ReviewIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True, template_name='review_text.html')
+    titulo = indexes.CharField(model_attr='tittle')
+    fecha = indexes.DateTimeField(model_attr='date')
+
+    def get_model(self):
+        return Gamereview
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
