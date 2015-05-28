bafa35a5109c84406b7c3b2e43a8a9f20a46d0e0
Fede Henzenn <fede.henzenn@gmail.com>
2015-05-28 20:47:00 -0300

    correccion error

--- forum/search_indexes.py
+++ forum/search_indexes.py
@@ -4,7 +4,7 @@ from forum.models import Gamereview
 
 class ReviewIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True, template_name='review_text.html')
-    titulo = indexes.CharField(model_attr='tittle')
+    titulo = indexes.CharField(model_attr='title')
     fecha = indexes.DateTimeField(model_attr='date')
 
     def get_model(self):
