b2194a2ced51ab093286ed1ad259198e917c275a
Fede Henzenn <fede.henzenn@gmail.com>
2015-06-01 17:25:47 -0300

    corregir

--- iwdehe/urls.py
+++ iwdehe/urls.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^search/', include('haystack.urls')),
+    url(r'^search/', include('haystack.urls'), name="search"),
     url(r'^$', 'sitio.views.home', name='nada'),
     url(r'^home/$', 'sitio.views.home', name='home'),
     url(r'^home/account/', include('allauth.urls')),
